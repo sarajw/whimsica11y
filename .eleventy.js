8078d728a67d65ec7991dff0b066cc2d8f908fd2
SJW <88535714+sarajw@users.noreply.github.com>
2024-06-14 13:43:04 +0200

    IDs, links

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,15 @@
-const markdownIt = require("markdown-it");
-const markdownItNamedHeadings = require("markdown-it-named-headings");
-
-const markdownOptions = {
-    html: true,
-    breaks: true,
-    linkify: true
-};
-const markdownRenderer = markdownIt(markdownOptions).use(markdownItNamedHeadings);
-
 module.exports = function(eleventyConfig) {
+
+  const markdownIt = require("markdown-it");
+  const markdownItNamedHeadings = require("markdown-it-named-headings");
+  const markdownOptions = {
+      html: true,
+      breaks: true,
+      linkify: true
+  };
+  const markdownRenderer = markdownIt(markdownOptions).use(markdownItNamedHeadings);
+  eleventyConfig.setLibrary("md", markdownRenderer);
+
   eleventyConfig.addPassthroughCopy("style.css");
   eleventyConfig.addPassthroughCopy("assets");
-  eleventyConfig.setLibrary("md", markdownRenderer);
 };
---
+++ links.md
@@ -0,0 +1,6 @@
+---
+title: Links
+layout: whimsica11yLayout.html
+---
+
+There will be links here - lots of good ones :)
